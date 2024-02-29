c68a48ec1ac0f5ecded2f5d98b1d988c0adb6a1f
NastyaNikiforova <danilova-anastaska@yandex.ru>
2024-02-29 19:30:02 +0300

    добавлен тест

---
+++ src/test/java/com/example/CatTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CatTest {
+
+    @org.junit.jupiter.api.Test
+    void getSound() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFood() {
+    }
+}
